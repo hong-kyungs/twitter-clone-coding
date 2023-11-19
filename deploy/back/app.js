686fc759bff0088c9bd266f450cdfce3be957215
hong-kyungs <hkyungs11@naver.com>
2023-11-19 15:03:26 +0900

    deploy: port 3060 -> 80

--- deploy/back/app.js
+++ deploy/back/app.js
@@ -39,7 +39,7 @@ if (process.env.NODE_ENV === 'production') {
 app.use(
 	cors({
 		//*로 모두 다 허용해줬지만 실무에서는 실제로 요청이 허용될 주소를 넣어준다.
-		origin: ['http://localhost:3060', 'nodebird.com'],
+		origin: ['http://localhost:3060', 'nodebird.com', 'http://3.35.9.197'],
 		credentials: true,
 	})
 );
@@ -74,6 +74,6 @@ app.use('/post', postRouter);
 app.use('/user', userRouter);
 app.use('/hashtag', hashtagRouter);
 
-app.listen(3065, () => {
+app.listen(80, () => {
 	console.log('서버 실행 중');
 });
